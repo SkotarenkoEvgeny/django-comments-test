804c19b99b1ff6675a3aa94c3ad7cce33ed5e4ac
yevhen_skotarenko <jeny797@gmail.com>
2026-07-21 20:45:18 +0300

    Add production Docker configuration

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -184,6 +184,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # =========================
 # MEDIA
--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -2,3 +2,9 @@ from .base import *
 
 
 DEBUG = False
+
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = "DENY"
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
